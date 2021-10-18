e518473ef49fdb941c7512fbf80c39eb86881f0c
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-10-18 23:11:02 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(moneyManagement());
+}
+
+class moneyManagement extends StatelessWidget {
+  const moneyManagement({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
