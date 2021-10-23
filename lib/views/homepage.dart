cbeb82922e061c8a9faf06aa3ee50899ff7440b4
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-10-23 15:07:31 +0600

    added operation Card

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -225,8 +225,90 @@ class _HomepageState extends State<Homepage> {
                 ],
               ),
             ),
+
+            Container(
+              height: 123,
+              child: ListView.builder(
+                itemCount: datas.length,
+                padding: EdgeInsets.only(left: 16),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        current = index;
+                      });
+                    },
+                    child: OperationCard(
+                        operation: datas[index].name,
+                        selectedIcon: datas[index].selectedIcon,
+                        unselectedIcon: datas[index].unselectedIcon,
+                        isSelected: current == index,
+                        context: this),
+                  );
+                },
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+class OperationCard extends StatefulWidget {
+  final String operation;
+  final String selectedIcon;
+  final String unselectedIcon;
+  final bool isSelected;
+  _HomepageState context;
+
+  OperationCard(
+      {this.operation,
+        this.selectedIcon,
+        this.unselectedIcon,
+        this.isSelected,
+        this.context});
+
+  @override
+  _OperationCardState createState() => _OperationCardState();
+}
+
+class _OperationCardState extends State<OperationCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 16),
+      width: 123,
+      height: 123,
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: kTenBlackColor,
+              blurRadius: 10,
+              spreadRadius: 5,
+              offset: Offset(8.0, 8.0),
+            )
+          ],
+          borderRadius: BorderRadius.circular(15),
+          color: widget.isSelected ? kBlueColor : kWhiteColor),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SvgPicture.asset(
+              widget.isSelected ? widget.selectedIcon : widget.unselectedIcon),
+          SizedBox(
+            height: 9,
+          ),
+          Text(
+            widget.operation,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.inter(
+                fontSize: 15,
+                fontWeight: FontWeight.w700,
+                color: widget.isSelected ? kWhiteColor : kBlueColor),
+          )
+        ],
       ),
     );
   }
